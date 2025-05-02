28bc6ff5a5e31f3407f792030a5ac208dd3e11e9
Silbad <psilbad@gmail.com>
2025-05-02 23:58:01 +0200

    refactor: simplify background script logging

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,5 +1,3 @@
 export default defineBackground(() => {
-	// This is the background script entry point
-	// You can use this to set up listeners, handle events, etc.
-	console.log("Background script loaded");
+	console.log("Hello background!", { id: browser.runtime.id });
 });
